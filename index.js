24e0c1fce1e0d880839ba7c1b077feb814294de6
unknown <xiongjoh@gmail.com>
2020-12-03 14:20:18 -0600

    add dynamic port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // code away!
 
+const port = process.env.PORT || 4000
+
 const server = require('./server')
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n');
   });
   
